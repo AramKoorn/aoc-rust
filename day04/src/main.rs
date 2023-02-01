71545d4552f2a6736cf785db198e9ff545bbded1
Aram Koorn <aram.koorn@nielsen.com>
2023-02-01 22:26:35 +0100

    day 04

---
+++ day04/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let mut f = include_str!("input.txt");
+    let mut lines: Vec<&str> = f.split("\n").collect();
+
+    let mut res: u32 = 0;
+    for l in lines {
+        let mut  x: Vec<&str> = l.split(",").collect();
+        let mut first = x[0];
+        let mut second = x[1];
+
+        let left: Vec<i32> = first.split('-').map(|x| x.parse().unwrap()).collect();
+        let right: Vec<i32> = second.split('-').map(|x| x.parse().unwrap()).collect();
+
+        // println!("{:?} {:?}", left, right);
+        if left[0] >= right[0] && left[1] <= right[1]|| right[0] >= left[0] && right[1] <= left[1]{
+            res += 1;
+        }
+    }
+    println!("{}", res);
+}
