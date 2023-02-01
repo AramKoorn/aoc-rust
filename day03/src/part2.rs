b7acf1f617791a7e33d94d86a09deca12f559d55
Aram Koorn <aram.koorn@nielsen.com>
2023-02-01 21:32:10 +0100

    remove commented lines

--- day03/src/part2.rs
+++ day03/src/part2.rs
@@ -36,38 +36,4 @@ pub fn part2() {
     println!("{:?}", chunks);
     let sum: i32 = chunks.iter().sum();
     println!("Answer part2: {}", sum);
-
-    // for l in lines {
-    //     let(mut left, mut right) = l.split_at(l.len() / 2);
-    //     println!("{} {}", left, right);
-    //     // println!("Common chars {}", common_chars(left, right));
-
-    //     let set1: HashSet<char> = left.chars().collect();
-    //     let set2: HashSet<char> = right.chars().collect();
-    //     let inter: HashSet<char> = set1.intersection(&set2).cloned().collect();
-    //     println!("{:?}", inter);
-
-    //     for x in inter {
-    //         v.push(x);
-    //     }
-    // }
-
-    // let mut t: i32 = 0;
-
-    // for s in v {
-
-    //     if s.is_lowercase() {
-    //         let mut x = s as i32 - 96; 
-    //         t += x;
-    //         println!("{}", x);
-    //     }
-    //     else {
-    //         let mut x = s as i32 - 64 + 26; 
-    //         println!("{}", x);
-    //         t += x;
-    //     }
-    // }
-
-    // println!("{}", t);
-
 }
