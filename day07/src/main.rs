e262ba831e135047a84a431a55315fac2ae8aecc
Aram Koorn <aram.koorn@icloud.com>
2023-02-20 17:15:22 +0100

    remove redundant print statements

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -9,13 +9,11 @@ fn main() {
 
     for cmd in commands[1..commands.len()].into_iter() {
         let mut s: Vec<&str> = cmd.split(" ").collect();
-        // println!{"{}", s[0]};
 
         if s[0] == "$" {
             if s[1] == "cd" {
                 // set current path 1 directory back
                 if s[2] == ".." {
-                    // println!("current {}", path);
                     let mut paths: Vec<&str> = path.split("/").collect();
                     let mut slices = &paths[1..paths.len() - 2];
                     let mut new_path: String = "/".to_string();
@@ -24,11 +22,9 @@ fn main() {
                         new_path += "/";
                     }
                     path = new_path;
-                    // println!("one back{}", path);
                 } else {
                     path += s[2];
                     path += "/";
-                    // println!{"{}", path};
                 }
             }
         } else if s[0] != "dir" {
@@ -47,33 +43,25 @@ fn main() {
         for sep in sepa.iter().skip(1).take(sepa.len() - 2) {
             tmp += sep;
             let key = tmp.clone();
-            // println!("{:?}", fs);
             let val = system.entry(key).or_insert(0);
             *val += size;
-
-            println!("{}", path);
         }
         let val = system.entry("/".to_string()).or_insert(0);
         *val += size;
-
     }
-    println! {"System: {:?}", system};
+    // println! {"System: {:?}", system};
 
     let mut t = 0;
     let limit: u32 = 100000;
     for (p, filesize) in system.iter() {
         if filesize <= &limit {
-            println!("{}", filesize);
             t += filesize;
         }
     }
 
-    println!("{:?}", fs);
     println!("Answer1: {}", t);
     let used_space = system.get(&"/".to_string()).unwrap();
     let used_space = *used_space as i32;
-
-    println!("Total used space: {}", used_space);
 
     // Part 2
     let total_size: i32 = 70_000_000;
@@ -82,7 +70,6 @@ fn main() {
     let mut v: Vec<i32> = Vec::new();
 
     for val in system.values() {
-
         let new_val = *val as i32;
 
         let tmp: i32 = (total_size - used_space) + new_val;
@@ -92,6 +79,4 @@ fn main() {
     }
     v.sort();
     println!("Answer part2: {:?}", v[0]);
-
-
 }
